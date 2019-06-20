5651a1a6fd519b3b81c2d330fb7d73a912466afe
Leila Weiss <leila.weiss@powerhrg.com>
2019-06-20 11:17:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+  yearsExperienceFromBeginningOf(year) {
+    return year - `${this.startDate.getFullYear()}`;
+  }
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+  avenueInteger(avenue){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    return eastWest.indexOf(avenue)
+  }
+  blocksTravelled(){
+    let horizontal = this.avenueInteger(this.endingLocation.horizontal) - this.avenueInteger(this.beginningLocation.horizontal)
+    let vertical = this.endingLocation.vertical - this.beginningLocation.vertical
+    return horizontal + vertical
+  }
+  estimatedTime(peak){
+    if (peak) {
+      return this.blocksTravelled() / 2
+    }
+    else {
+      return this.blocksTravelled() / 3
+    }
+  }
+}
